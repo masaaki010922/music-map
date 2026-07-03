0823967bc14b412ee861627223853df0a1527cb1
masaaki010922 <elesag110@gmail.com>
2026-07-03 12:33:20 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,42 @@
+const albums = [
+  {
+    title: "Loveless",
+    artist: "My Bloody Valentine",
+    year: 1991,
+    genre: "Shoegaze",
+    x: 72,
+    y: 38,
+    influence: 95,
+    note: "ギターを電子音響的に扱った名盤。"
+  },
+  {
+    title: "Voodoo",
+    artist: "D'Angelo",
+    year: 2000,
+    genre: "R&B / Neo Soul",
+    x: 32,
+    y: 45,
+    influence: 90,
+    note: "生演奏的だが、編集感とグルーヴ設計も強い。"
+  },
+  {
+    title: "Discovery",
+    artist: "Daft Punk",
+    year: 2001,
+    genre: "House / French Touch",
+    x: 86,
+    y: 76,
+    influence: 88,
+    note: "クラブミュージックをポップに接続した作品。"
+  },
+  {
+    title: "Blue",
+    artist: "Joni Mitchell",
+    year: 1971,
+    genre: "Folk / Singer-songwriter",
+    x: 18,
+    y: 22,
+    influence: 86,
+    note: "私的で、言葉と旋律の比重が高い。"
+  }
+];
